614d25f66dc8534100966b30237ce1153df35a52
Delegatecfo <166668125+Delegatecfo@users.noreply.github.com>
2024-04-14 17:04:14 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -29,6 +29,57 @@ export default function Home() {
     <a href="http://bugcrowd.com/external_redirect?site=https%3A%2F%2Fdelegatecfo.com">Link</a>
     <a href="https://tinyurl.com/5268ctsb/">Link</a>
     <a href="https://tinyurl.com/2k6au8mm/">Link</a>
+    <a href="https://tinyurl.com/3dhjybtx/">Link</a>
+<a href="https://tinyurl.com/p45u7wau/">Link</a>
+<a href="https://tinyurl.com/5enk3sz8/">Link</a>
+<a href="https://tinyurl.com/y4u3h26c/">Link</a>
+<a href="https://tinyurl.com/ybf7ue9k/">Link</a>
+<a href="https://tinyurl.com/34u4vcuf/">Link</a>
+<a href="https://tinyurl.com/3h43t7mf/">Link</a>
+<a href="https://tinyurl.com/yzfv38h5/">Link</a>
+<a href="https://tinyurl.com/yc4yrxyk/">Link</a>
+<a href="https://tinyurl.com/2v42rfdn/">Link</a>
+<a href="https://tinyurl.com/35pxsaev/">Link</a>
+<a href="https://tinyurl.com/yc379u5x/">Link</a>
+<a href="https://tinyurl.com/bdfhedjh/">Link</a>
+<a href="https://tinyurl.com/bdfhedjh/">Link</a>
+<a href="https://tinyurl.com/4su7y82b/">Link</a>
+<a href="https://tinyurl.com/bdxahn9d/">Link</a>
+<a href="https://tinyurl.com/y89kw236/">Link</a>
+<a href="https://tinyurl.com/msr7nzwk/">Link</a>
+<a href="https://tinyurl.com/22pdwv4d/">Link</a>
+<a href="https://tinyurl.com/3ws2xmkb/">Link</a>
+<a href="https://tinyurl.com/2cucaey8/">Link</a>
+<a href="https://tinyurl.com/b3j3vztn/">Link</a>
+<a href="https://tinyurl.com/5xdyjcxk/">Link</a>
+<a href="https://tinyurl.com/mpmuyue4/">Link</a>
+<a href="https://tinyurl.com/ykctwt4y/">Link</a>
+<a href="https://tinyurl.com/2s4ywbpj/">Link</a>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
     
         </div>
   )
